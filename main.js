2ef4bbff20166d531c78911dd666cdf14f5d9422
web3genie <wxzvrd@protonmail.com>
2024-10-22 16:32:45 +0100

    how about now?

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'; // Make sure to use the correct path for Three.js controls
 
 // Setup
 
